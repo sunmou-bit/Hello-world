1ec89d82c0a8abf593651e47acbdd26e7e4d0daf
SunMou <sunmou_bit@163.com>
2017-12-22 13:35:16 +0800

    Initial commit

---
+++ src/com/sunmou/demo/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.sunmou.demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.print("++++++++++++");
+       }
+}
